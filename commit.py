f90859d8852b60d8f1b0f5b36abf39eacad4c878
meeboo3 <118372959+meeboo3@users.noreply.github.com>
2023-01-25 19:08:48 +0800

    Update commit.py

--- commit.py
+++ commit.py
@@ -10,5 +10,5 @@ time = now.strftime('%y-%m-%d-%H-%M-%S')
 
 subprocess.run('git config --global user.email noreply@arkfans.top', shell=True, cwd=base)
 subprocess.run('git config --global user.name MeeBooBot_v0', shell=True, cwd=base)
-subprocess.run('add .', shell=True, cwd=base)
+subprocess.run('git add .', shell=True, cwd=base)
 subprocess.run(f'git commit -m \'[UPDATE] Data:{now}-{os.environ["version"]}\'', shell=True, cwd=base)
